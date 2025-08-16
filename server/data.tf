d5a50a25d358139b838293c3b9a5cf59fb0eee1d
Jörg Henning <1712460+joerx@users.noreply.github.com>
2025-08-16 20:44:50 +0200

    Support for 1.21.8 (#2)
    Adding download URL for 1.21.8

--- server/data.tf
+++ server/data.tf
@@ -4,6 +4,7 @@ locals {
     "1.19.3" = "https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar"
     "1.19.4" = "https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar"
     "1.21.7" = "https://piston-data.mojang.com/v1/objects/05e4b48fbc01f0385adb74bcff9751d34552486c/server.jar"
+    "1.21.8" = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
   }
 
   minecraft_port = "25565"
